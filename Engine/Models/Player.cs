cb4664117b4dffe02e1905194411de38bef7ffab
Pete <SilurusPete@gmail.com>
2026-05-22 15:39:27 +0300

    Added Player Class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string Nickname {  get; set; }
+        string CharacterClass { get; set; }
+        int HP { get; set; }
+        int Exp { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+    }
+}
