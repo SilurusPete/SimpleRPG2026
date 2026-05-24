b5e57aa921d7a257c77c2776b80309026a0e558b
Emerugar <ivanr301207@gmail.com>
2026-05-25 01:08:58 +0300

    ProblemSolved
    Enemy Image problem solved

--- Engine1/Models/Monster.cs
+++ Engine1/Models/Monster.cs
@@ -29,7 +29,8 @@ namespace Engine.Models
             int rewardExperiencePoints, int rewardGold)
         {
             Name = name;
-            ImageName = string.Format("pack://application:,,,/Engine;component/Images/Monsters/{0}", imageName); MaximumHitPoints = maximumHitPoints;
+            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+            MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
